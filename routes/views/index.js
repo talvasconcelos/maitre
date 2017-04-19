d9b196a7dace5d4d3d8163e7463e53e420fc82d9
Tiago Prosp <tiago@prosp>
2017-04-19 18:48:02 +0100

    index half way

--- routes/views/index.js
+++ routes/views/index.js
@@ -5,16 +5,13 @@ exports = module.exports = function (req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	//locals.filter = [];
-
-
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
 
-	// Load the posts
+	view.query('cidades', keystone.list('Restaurant').model.find().distinct('location.state'));
 	view.query('restaurants', keystone.list('Restaurant').model.find());
-	
+
 	// Render the view
 	view.render('index');
 };
